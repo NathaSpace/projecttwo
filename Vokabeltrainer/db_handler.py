252bcd637091e8d8d8ca3a763514a8f28d3cde59
NathaSpace <nkus@stud.macromedia.de>
2024-01-24 18:38:29 +0700

    db_commit

--- Vokabeltrainer/db_handler.py
+++ Vokabeltrainer/db_handler.py
@@ -1,12 +1,11 @@
-def reset_database():
-    confirm = input("Möchtest du die Datenbank zurücksetzen? (ja/nein): ")
-    if confirm.lower() == 'ja':
-        conn = sqlite3.connect('benutzerdaten.db')
-        cursor = conn.cursor()
-        cursor.execute('DELETE FROM benutzer')
-        conn.commit()
-        print("Datenbank wurde erfolgreich zurückgesetzt.")
-        conn.close()
-    else:
+def reset_database():                                                                   #Definition der Funktion reset_database
+    confirm = input("Möchtest du die Datenbank zurücksetzen? (ja/nein): ")              #Benutzerabfrage für das Zurücksetzen der Datenbank
+    if confirm.lower() == 'ja':                                                         #Eingabe ja?
+        conn = sqlite3.connect('benutzerdaten.db')                                      #Verbindung zur SQLite-Datenbank herstellen
+        cursor = conn.cursor()                                                          #Cursor erstellen, um mit der Datenbank zu interagieren
+        cursor.execute('DELETE FROM benutzer')                                          #SQL-Befehl zum Löschen aller Einträge in der Tabelle 'benutzer'
+        conn.commit()                                                                   #Änderungen in der Datenbank bestätigen
+        print("Datenbank wurde erfolgreich zurückgesetzt.")                             #Print Bestätigung
+        conn.close()                                                                    #Verbindung schließen
+    else:                                                                               #Wenn nicht ja, dann nicht zurücksetzen
         print("Nicht zurückgesetzt.")
-        
